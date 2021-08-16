700a6d42c3171697d6357c3bef9a9e2a9cbc243a
Viviana Camacho <viviana.camacho@jalasoft.com>
2021-08-16 16:09:57 -0400

    Boolean primitives and some fmting

--- src/github.com/vivisima/firstapp/Main.go
+++ src/github.com/vivisima/firstapp/Main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Go!")
+
+	/* files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	} */
+	//var i int
+	//i = 42
+	//var j int = 27
+	var l bool //all initialized variables have a 0 value
+	k := 99
+	fmt.Printf("%v, %T\n", k, k)
+	n := 1 == 1
+	m := 1 == 2
+	fmt.Printf("%v, %T\n", n, n)
+	fmt.Printf("%v, %T\n", m, m)
+	fmt.Printf("%v, %T\n", l, l)
 }
