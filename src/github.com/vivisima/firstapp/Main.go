6750cf423b5d7a297c8b0799896544e717ef479d
Viviana Camacho <viviana.camacho@jalasoft.com>
2021-08-17 16:06:06 -0400

    on constants

--- src/github.com/vivisima/firstapp/Main.go
+++ src/github.com/vivisima/firstapp/Main.go
@@ -4,28 +4,11 @@ import (
 	"fmt"
 )
 
+const b = iota
+
 func main() {
-	a := 10 //1010
-	b := 3  //0011
-	fmt.Println(a + b)
-	fmt.Println(a - b)
-	fmt.Println(a * b)
-	fmt.Println(a / b)
-	fmt.Println(a % b)
-	fmt.Println(float32(a) / float32(b)) //type conversion
-
-	//bit operators
-	fmt.Println(a & b)  //0010 and
-	fmt.Println(a | b)  //1011 or
-	fmt.Println(a ^ b)  //1001 xor
-	fmt.Println(a &^ b) //and not .. 0100
-
-	//bit shifting
-	c := 8              //2^3
-	fmt.Println(c << 3) // 2^3 * 2^3 = 2^6  shift to the left
-	fmt.Println(c >> 3) // 2^3 / 2^3 = 2^0 shift to the right
-
-	// text types string: unicode8 char, runes
-	s := "this is a string"
-	fmt.Printf("%v, %T\n", s[2], s[2])
+	// CONSTANTS, can be shadowed
+	const myConst int = 42 // MyConst --> this will be automatically exported
+	var b int = 27
+	fmt.Printf("%v, %T\n", myConst+b, myConst+b)
 }
